701388c39adfb45247ef3173db8c4f0bae8d4cd3
BRUNO OLIVEIRA <brunnomoraaes@gmail.com>
2022-09-15 17:22:46 -0300

    criando pagina chucknorris

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import ChuckNorris from './components/chuckNorris';
+import DadJoke from './components/dadJoke';
 
 function App() {
-
-  const [joke, setJoke] = useState("jokes here")
-
-  useEffect(() => {
-    getJoke()
-  }, []);
-
-  const getJoke = async () => {
-    const response = await fetch("https://icanhazdadjoke.com/", {
-      headers: {
-        "Accept": "application/json",
-      },
-    });
-    const data = await response.json();
-    setJoke(data.joke);
-  }
-
   return (
-    <div className="container">
-      <h1>Don't laugh challenge</h1>
-      <p className="joke">{joke}</p>
-      <button className="btn" onClick={getJoke}>Get another joke</button>
-    </div>
-  );
+    <>
+      <ChuckNorris />
+    </>
+  )
 }
 
-export default App;
+export default App
