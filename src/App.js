edc8c12da8e92d362c43d7cec90b1228f6d7979e
BRUNO OLIVEIRA <brunnomoraaes@gmail.com>
2022-09-15 16:29:31 -0300

    finalizando estilizacao

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import { useState } from "react";
 
 function App() {
 
-  const [joke, setJoke] = useState("piada aqui")
+  const [joke, setJoke] = useState("jokes here")
 
   useEffect(() => {
-    
-  }, [])
+    getJoke()
+  }, []);
 
   const getJoke = async () => {
-    const response = await fetch('https://icanhazdadjoke.com/');
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        "Accept": "application/json",
+      },
+    });
+    const data = await response.json();
+    setJoke(data.joke);
   }
 
   return (
     <div className="container">
-      <h1>Desafio: não ria!</h1>
+      <h1>Don't laugh challenge</h1>
       <p className="joke">{joke}</p>
-      <button className="btn">Trocar piada</button>
+      <button className="btn" onClick={getJoke}>Get another joke</button>
     </div>
   );
 }
